cd1c9773a455dbf20d1caf60810b903b61073021
DEV-LUK4Z <lucassoares1099@outlook.com>
2023-09-27 11:54:37 -0300

    remoção de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const h1 = document.getElementById('h1');
 const banner = document.getElementById('banner');
-const button = document.getElementById('button');
 
 const onScroll = (event) => {
         const scrollPosition = event.target.scrollingElement.scrollTop;
@@ -11,20 +10,12 @@ const onScroll = (event) => {
                 h1.style.opacity = 0;
                 h1.style.translate = '0 -50px';
                 h1.style.scale = '0.9';
-
-                button.style.opacity = 0;
-                button.style.translate = '0 -40%';
-                button.style.scale = '0.8';
         } else {
                 banner.style.backgroundSize = '180%';
 
                 h1.style.opacity = 1;
                 h1.style.translate = 0;
                 h1.style.scale = 1;
-
-                button.style.opacity = 1;
-                button.style.translate = 0;
-                button.style.scale =1;
         }
 };
 
